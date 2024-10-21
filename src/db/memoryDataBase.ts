045a7c94465490807118642abfa047ccd979fffc
antonsokolovsky <anton.a.sokolovsky@gmail.com>
2024-10-21 08:28:25 +0300

    feat: add memory user in data base

---
+++ src/db/memoryDataBase.ts
@@ -0,0 +1,42 @@
+import { v4 as uuidv4 } from 'uuid';
+import { User } from '../types/user';
+
+let users: User[] = [];
+
+export const getUsers = (): User[] => users;
+
+export const getUserById = (id: string): User | undefined =>
+  users.find((user) => user.id === id);
+
+export const createUser = (
+  username: string,
+  age: number,
+  hobbies: string[],
+): User => {
+  const newUser = { id: uuidv4(), username, age, hobbies };
+  users.push(newUser);
+  return newUser;
+};
+
+export const deleteUser = (id: string): boolean => {
+  const userIndex = users.findIndex((user) => user.id === id);
+  if (userIndex !== -1) {
+    users.splice(userIndex, 1);
+    return true;
+  }
+  return false;
+};
+
+export const updateUser = (
+  id: string,
+  username: string,
+  age: number,
+  hobbies: string[],
+): User | undefined => {
+  const userIndex = users.findIndex((user) => user.id === id);
+  if (userIndex !== -1) {
+    users[userIndex] = { id, username, age, hobbies };
+    return users[userIndex];
+  }
+  return undefined;
+};
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,25 +1,68 @@
 import { IncomingMessage, ServerResponse } from 'http';
+import {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} from '../db/memoryDataBase';
 import { HttpStatusCode } from '../constants/statusCodes';
 import { HttpMethod } from '../constants/methods';
 import { ResponseMessage } from '../constants/responseMessage';
 
-export const handleUserRoutes = (req: IncomingMessage, res: ServerResponse) => {
+export const handleUserRoutes = async (
+  req: IncomingMessage,
+  res: ServerResponse,
+) => {
   const url = req.url;
   const method = req.method;
 
   if (url?.startsWith('/api/users')) {
-    if (method === HttpMethod.GET) {
+    const userId = url.split('/')[3];
+
+    if (method === HttpMethod.GET && !userId) {
       res.statusCode = HttpStatusCode.OK;
-      res.end(ResponseMessage.FETCHING);
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(getUsers()));
+    } else if (method === HttpMethod.GET && userId) {
+      const user = getUserById(userId);
+      if (user) {
+        res.statusCode = HttpStatusCode.OK;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(user));
+      } else {
+        res.statusCode = HttpStatusCode.NOT_FOUND;
+        res.end(ResponseMessage.USER_NOT_FOUND);
+      }
     } else if (method === HttpMethod.POST) {
-      res.statusCode = HttpStatusCode.CREATED;
-      res.end(ResponseMessage.USER_CREATED);
-    } else if (method === HttpMethod.PUT) {
-      res.statusCode = HttpStatusCode.OK;
-      res.end(ResponseMessage.USER_UPDATED);
-    } else if (method === HttpMethod.DELETE) {
-      res.statusCode = HttpStatusCode.NO_CONTENT;
-      res.end(ResponseMessage.USER_DELETED);
+      let body = '';
+      req.on('data', (chunk) => (body += chunk));
+      req.on('end', () => {
+        const { username, age, hobbies } = JSON.parse(body);
+        const newUser = createUser(username, age, hobbies);
+        res.statusCode = HttpStatusCode.CREATED;
+        res.end(JSON.stringify(newUser));
+      });
+    } else if (method === HttpMethod.PUT && userId) {
+      let body = '';
+      req.on('data', (chunk) => (body += chunk));
+      req.on('end', () => {
+        const { username, age, hobbies } = JSON.parse(body);
+        const updatedUser = updateUser(userId, username, age, hobbies);
+        if (updatedUser) {
+          res.statusCode = HttpStatusCode.OK;
+          res.end(JSON.stringify(updatedUser));
+        } else {
+          res.statusCode = HttpStatusCode.NOT_FOUND;
+          res.end(ResponseMessage.USER_NOT_FOUND);
+        }
+      });
+    } else if (method === HttpMethod.DELETE && userId) {
+      const deleted = deleteUser(userId);
+      res.statusCode = deleted
+        ? HttpStatusCode.NO_CONTENT
+        : HttpStatusCode.NOT_FOUND;
+      res.end();
     } else {
       res.statusCode = HttpStatusCode.METHOD_NOT_ALLOWED;
       res.end(ResponseMessage.METHOD_NOT_ALLOWED);
